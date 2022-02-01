cac7c90e5c54ccded0eff20d2d4ba720a1c4ec60
julio <juliodbarriga@gmail.com>
2022-01-31 20:07:05 -0600

    Initialized AuthorController and assigned mapping.

--- src/main/java/com/library/libraryapi/controller/AuthorController.java
+++ src/main/java/com/library/libraryapi/controller/AuthorController.java
@@ -1,2 +1,11 @@
-package com.library.libraryapi.controller;public class AuthorController {
+package com.library.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class AuthorController {
+
+    
 }
