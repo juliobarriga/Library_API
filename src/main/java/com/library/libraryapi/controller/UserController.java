c4c1f4f33b5308800fed84ef1127856d7d1b9338
julio <juliodbarriga@gmail.com>
2022-02-01 11:14:05 -0600

    Created resiterUser() test endpoint.

--- src/main/java/com/library/libraryapi/controller/UserController.java
+++ src/main/java/com/library/libraryapi/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.library.libraryapi.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth/users/")
 public class UserController {
+
+    @PostMapping("/register")
+    public String registerUser(){
+        return "calling registerUser";
+    }
 }
