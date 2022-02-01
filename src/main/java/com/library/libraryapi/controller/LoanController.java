c115cc08611ce492d817d7968e9f80df7d5cb66a
julio <juliodbarriga@gmail.com>
2022-02-01 10:34:02 -0600

    Initialized LoanController.

--- src/main/java/com/library/libraryapi/controller/LoanController.java
+++ src/main/java/com/library/libraryapi/controller/LoanController.java
@@ -1,4 +1,9 @@
 package com.library.libraryapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
 public class LoanController {
 }
