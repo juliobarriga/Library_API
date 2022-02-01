9b5be3c62b9ab2ad1241bde59514dc9811da8e5b
julio <juliodbarriga@gmail.com>
2022-02-01 10:35:32 -0600

    Created getAllLoans() test endpoint.

--- src/main/java/com/library/libraryapi/controller/LoanController.java
+++ src/main/java/com/library/libraryapi/controller/LoanController.java
@@ -1,9 +1,15 @@
 package com.library.libraryapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/")
 public class LoanController {
+
+    @GetMapping("/loans/")
+    public String getAllLoans(){
+        return "calling getAllLoans";
+    }
 }
