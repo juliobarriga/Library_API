401ef46f9644d2dd587be207a63e76889ab3f1be
julio <juliodbarriga@gmail.com>
2022-01-31 19:44:52 -0600

    Commented out hello-world endpoint and created getAllBooks() test endpoint.

--- src/main/java/com/library/libraryapi/controller/BookController.java
+++ src/main/java/com/library/libraryapi/controller/BookController.java
@@ -8,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/")
 public class BookController {
 
-    @GetMapping("/hello-world/")
-    public String getHelloWorld(){
-        return "Hello World";
+//    @GetMapping("/hello-world/")
+//    public String getHelloWorld(){
+//        return "Hello World";
+//    }
+    @GetMapping("/books/")
+    public String getAllBooks(){
+        return "calling getAllBooks";
     }
 }
