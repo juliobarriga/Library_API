8255194a1b1a166e7d537918f0243edc5a6c51ce
julio <juliodbarriga@gmail.com>
2022-02-01 16:09:26 -0600

    Added exception info and behabior.

--- src/main/java/com/library/libraryapi/exceptions/IncompleteInformationException.java
+++ src/main/java/com/library/libraryapi/exceptions/IncompleteInformationException.java
@@ -1,2 +1,9 @@
-package com.library.libraryapi.exceptions;public class IncompleteInformationException {
+package com.library.libraryapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class IncompleteInformationException extends RuntimeException{
+    public IncompleteInformationException(String message){super(message);}
 }
