d2f9bf6bf64f42a97fa8061253865a448b04420f
julio <juliodbarriga@gmail.com>
2022-02-01 10:59:28 -0600

    Initialized UserController.

--- src/main/java/com/library/libraryapi/controller/UserController.java
+++ src/main/java/com/library/libraryapi/controller/UserController.java
@@ -1,2 +1,9 @@
-package com.library.libraryapi.controller;public class UserController {
+package com.library.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth/users/")
+public class UserController {
 }
