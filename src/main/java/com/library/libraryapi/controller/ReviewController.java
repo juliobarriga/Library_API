02e465d58b0565b2fe3ae7c6026d4c5e6b5ab42f
julio <juliodbarriga@gmail.com>
2022-02-01 09:14:26 -0600

    Created getAllReviews() test endpoint.

--- src/main/java/com/library/libraryapi/controller/ReviewController.java
+++ src/main/java/com/library/libraryapi/controller/ReviewController.java
@@ -1,10 +1,16 @@
 package com.library.libraryapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/")
 public class ReviewController {
-    
+
+    @GetMapping("/reviews/")
+    public String getAllReviews(){
+        return "calling getAllReviews";
+    }
+
 }
