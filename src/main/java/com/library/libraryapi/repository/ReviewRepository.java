e6dc6325b9862fad8681d49e3ee4f27881195400
julio <juliodbarriga@gmail.com>
2022-02-01 12:26:45 -0600

    Created ReviewRepository.

---
+++ src/main/java/com/library/libraryapi/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.library.libraryapi.repository;
+
+import com.library.libraryapi.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
