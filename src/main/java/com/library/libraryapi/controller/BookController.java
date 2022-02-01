86a26d52a46448683366956b09a9109580956b4e
julio <juliodbarriga@gmail.com>
2022-01-31 18:28:47 -0600

    Created hello-world endpoint in BookController to test application.

--- src/main/java/com/library/libraryapi/controller/BookController.java
+++ src/main/java/com/library/libraryapi/controller/BookController.java
@@ -1,2 +1,15 @@
-package com.library.libraryapi.controller;public class BookController {
+package com.library.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class BookController {
+
+    @GetMapping("/hello-world/")
+    public String getHelloWorld(){
+        return "Hello World";
+    }
 }
