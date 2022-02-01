399fb122bd4df2f8f32a3c8c463c3e92a872774c
julio <juliodbarriga@gmail.com>
2022-01-31 19:22:10 -0600

    Created Review model with variables, constructors and setters and getters.

---
+++ src/main/java/com/library/libraryapi/model/Review.java
@@ -0,0 +1,70 @@
+package com.library.libraryapi.model;
+
+public class Review {
+
+    private Long id;
+    private String comment;
+    private Integer rating;
+    private Book book;
+    private User user;
+
+    public Review() {
+    }
+
+    public Review(Long id, String comment, Integer rating) {
+        this.id = id;
+        this.comment = comment;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", comment='" + comment + '\'' +
+                ", rating=" + rating +
+                ", book=" + book +
+                ", user=" + user +
+                '}';
+    }
+}
