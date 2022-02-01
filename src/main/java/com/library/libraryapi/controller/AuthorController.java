fc05911b01b1668a0c36485218e98bad80f44f4c
julio <juliodbarriga@gmail.com>
2022-01-31 20:11:15 -0600

    Created addNewAuthor() test endpoint.

--- src/main/java/com/library/libraryapi/controller/AuthorController.java
+++ src/main/java/com/library/libraryapi/controller/AuthorController.java
@@ -1,6 +1,7 @@
 package com.library.libraryapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class AuthorController {
     @GetMapping("/authors/")
     public String getAllAuthors(){
         return "calling getAllAuthors";
+    }
+
+    @PostMapping("/authors/")
+    public String addNewAuthor(){
+        return "calling addNewAuthor";
     }
 }
