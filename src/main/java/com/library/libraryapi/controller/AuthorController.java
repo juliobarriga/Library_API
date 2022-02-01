d19534f9814b9f5ef378029c9d8c7969a45fdc1e
julio <juliodbarriga@gmail.com>
2022-01-31 20:08:51 -0600

    Created getAllAuthors() test endpoint.

--- src/main/java/com/library/libraryapi/controller/AuthorController.java
+++ src/main/java/com/library/libraryapi/controller/AuthorController.java
@@ -1,5 +1,6 @@
 package com.library.libraryapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/")
 public class AuthorController {
 
-    
+    @GetMapping("/authors/")
+    public String getAllAuthors(){
+        return "calling getAllAuthors";
+    }
 }
