23cb7693cc8df6a53216af0886cd2a2094f751e8
julio <juliodbarriga@gmail.com>
2022-02-01 14:31:40 -0600

    Deleted AuthorRepository

--- src/main/java/com/library/libraryapi/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.libraryapi.repository;
-
-import com.library.libraryapi.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
