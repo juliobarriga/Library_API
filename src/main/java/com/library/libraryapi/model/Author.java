f0ee639fc7a58847a62679db5420f6539e63b361
julio <juliodbarriga@gmail.com>
2022-01-31 18:50:41 -0600

    Created Author model and added variables, constructors and setters and getters.

---
+++ src/main/java/com/library/libraryapi/model/Author.java
@@ -0,0 +1,60 @@
+package com.library.libraryapi.model;
+
+import java.util.List;
+
+public class Author {
+
+    private Long id;
+    private String name;
+    private String about;
+    private List<Book> bookList;
+
+    public Author() {
+    }
+    public Author(Long id, String name, String about) {
+        this.id = id;
+        this.name = name;
+        this.about = about;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", about='" + about + '\'' +
+                '}';
+    }
+}
