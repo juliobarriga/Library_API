ca8b4cfc752499ed677248c377fac63ff6c292d2
julio <juliodbarriga@gmail.com>
2022-02-02 16:24:05 -0600

    Created registerUser method.

--- src/main/java/com/library/libraryapi/service/UserService.java
+++ src/main/java/com/library/libraryapi/service/UserService.java
@@ -1,5 +1,7 @@
 package com.library.libraryapi.service;
 
+import com.library.libraryapi.exceptions.InformationExistException;
+import com.library.libraryapi.model.User;
 import com.library.libraryapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,5 +18,15 @@ public class UserService {
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User registerUser(User userObject){
+        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistException("user with email address " + userObject.getEmailAddress() +
+                    " already exists");
+        }
     }
 }
