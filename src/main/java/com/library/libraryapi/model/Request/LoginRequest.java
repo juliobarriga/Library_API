13d62f45ce326254eec52cfb8f566438595fe0dd
julio <juliodbarriga@gmail.com>
2022-02-02 17:07:58 -0600

    Created Request folder and added LoginRequest class.

---
+++ src/main/java/com/library/libraryapi/model/Request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.library.libraryapi.model.Request;
+
+public class LoginRequest {
+
+    private String emailAddress;
+    private String password;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
